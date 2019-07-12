1c79c913500d71efd39ff51247158ac0f68cb675
suve <veg@svgames.pl>
2019-07-12 16:10:21 +0200

    Specify a licence (GPL3 or later)

--- cpso.sh
+++ cpso.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# cpso.sh - copy program's dependencies (.so files)
+# Copyright (C) 2017 Artur "suve" Iwicki
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License,
+# either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program (LICENCE.txt). If not, see <http://www.gnu.org/licenses/>.
+#
 
 blacklist=("libasan." "libc." "libgcc_s." "libm." "libpthread." "libstdc++.")
 
