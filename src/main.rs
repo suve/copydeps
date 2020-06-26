3846c698edbf6622f646fa47b87f521a12ee3e5d
suve <veg@svgames.pl>
2020-06-26 14:46:52 +0200

    Move dep processing (copy/print) to separate file

--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,18 @@
  * You should have received a copy of the GNU General Public License along with
  * this program (LICENCE.txt). If not, see <https://www.gnu.org/licenses/>.
  */
-use std::collections::HashMap;
-use std::fs;
-use std::path::PathBuf;
 use std::process::exit;
 
 #[macro_use] extern crate lazy_static;
 
-extern crate same_file;
-use same_file::is_same_file;
-
 mod parser;
 use parser::get_deps;
 
+mod process_deps;
+use process_deps::copy_deps;
+use process_deps::print_deps;
+
 mod resolver;
-use resolver::Status;
-use resolver::resolve;
 use resolver::resolve_recursively;
 
 mod settings;
@@ -38,104 +34,6 @@ use settings::Settings;
 mod version;
 use version::*;
 
-fn dep_print(name: &String, status: &Status, _settings: &Settings) -> bool {
-	match status {
-		Status::Ignored => println!("\"{}\": (ignored)", name),
-		Status::FailedToResolve => println!("\"{}\": (failed to resolve)", name),
-		Status::Resolved(r) => println!("\"{}\": {}", name, r.to_string_lossy()),
-	}
-	return true;
-}
-
-fn should_copy(name: &String, source: &PathBuf, destination: &PathBuf, settings: &Settings) -> Result<bool, String> {
-	if !destination.exists() {
-		return Result::Ok(true);
-	}
-
-	if settings.no_clobber {
-		if settings.verbose {
-			println!("\"{}\": already exists in the target directory and --no-clobber was specified", name);
-		}
-		return Result::Ok(false);
-	}
-
-	match is_same_file(source, &destination) {
-		Ok(true) => {
-			if settings.verbose {
-				println!("\"{}\": preferred version already present in target directory", name);
-			}
-			return Result::Ok(false);
-		},
-		Err(err) => {
-			return Result::Err(format!(
-				"Failed to determine if \"{}\" and \"{}\" refer to the same file: {}",
-				source.to_string_lossy(), destination.to_string_lossy(), err
-			));
-		}
-		Ok(false) => { return Result::Ok(true); }
-	};
-}
-
-fn dep_copy(name: &String, status: &Status, settings: &Settings) -> bool {
-	match status {
-		Status::Ignored => {
-			if settings.verbose {
-				println!("\"{}\": ignored, skipping", name)
-			}
-			return true;
-		},
-		Status::FailedToResolve => {
-			eprintln!("{}: failed to resolve \"{}\"", PROGRAM_NAME, name);
-			return false;
-		},
-		Status::Resolved(resolved) => {
-			let mut destination = settings.target_dir.clone();
-			destination.push(name);
-
-			match should_copy(name, resolved, &destination, settings) {
-				Err(err) => {
-					eprintln!("{}: {}", PROGRAM_NAME, err);
-					return false;
-				},
-				Ok(false) => {
-					return true;
-				},
-				Ok(true) => {
-					match fs::copy(resolved, &destination) {
-						Ok(_) => {
-							if settings.verbose {
-								println!("\"{}\": {} -> {}", name, resolved.to_string_lossy(), destination.to_string_lossy())
-							}
-							return true;
-						},
-						Err(err) => {
-							eprintln!("{}: failed to copy \"{}\": {}", PROGRAM_NAME, name, err);
-							return false;
-						},
-					}
-				}
-			}
-		}
-	}
-}
-
-type DepCallback = fn(name: &String, status: &Status, settings: &Settings) -> bool;
-
-fn process_deps(deps: &HashMap<String, Status>, callback: DepCallback, settings: &Settings) -> bool {
-	let mut all_ok = true;
-
-	let mut sorted_keys = deps.keys().collect::<Vec<&String>>();
-	sorted_keys.sort();
-
-	for key in sorted_keys {
-		let val = deps.get(key.as_str()).unwrap();
-		if !callback(&key, val, settings) {
-			all_ok = false;
-		}
-	}
-
-	return all_ok;
-}
 
 fn main() {
 	let mut settings = match Settings::new_from_argv() {
@@ -158,6 +56,12 @@ fn main() {
 		Err(msg) => { eprintln!("{}: {}", PROGRAM_NAME, msg); exit(3); }
 	};
 
-	let status = process_deps(&deps, if settings.dry_run { dep_print } else { dep_copy }, &settings);
-	exit(if status { 0 } else { 4 });
+	let count = match settings.dry_run {
+		true => print_deps(&deps, &settings),
+		false => copy_deps(&deps, &settings),
+	};
+
+	if count.failed_to_resolve > 0 { exit(4); }
+	if count.failed_to_copy > 0 { exit(5); }
+	exit(0);
 }
